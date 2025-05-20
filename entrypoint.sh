3c7758b902b3427feafea00ea71b812e4c94bb7e
Louis Garman <louisgarman@gmail.com>
2025-05-20 15:23:07 +0100

    fix: check local dir first to avoid HTTP429

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,8 +25,8 @@
 
 # Prerequisites
 # - yq v4.6
-# - kustomize v4.1
-# - kubeconform v0.4
+# - kustomize v5.6.0
+# - kubeconform v0.7.0
 
 set -o errexit
 
@@ -42,7 +42,8 @@ find . -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
     yq e 'true' "$file" > /dev/null
 done
 
-kubeconform_config="-strict -ignore-missing-schemas -schema-location default -schema-location /tmp/flux-crd-schemas -verbose"
+mkdir /tmp/cache
+kubeconform_config="-strict -ignore-missing-schemas -cache /tmp/cache -schema-location /tmp/flux-crd-schemas -schema-location default -verbose"
 
 echo "INFO - Validating clusters"
 find ./clusters -maxdepth 2 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
